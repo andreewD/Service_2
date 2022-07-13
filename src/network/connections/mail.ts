7b313c50850d6997a55dd24621e17134f9db6296
andr33wD <hairton.anchi.d@uni.pe>
2022-07-13 00:04:54 -0500

    feat: prenotification service added

---
+++ src/network/connections/mail.ts
@@ -0,0 +1,19 @@
+import nodemailer from 'nodemailer'
+
+
+const transporter =nodemailer.createTransport({
+    host:'smtp.gmail.com',
+    port:465,
+    secure:true,
+    auth:{
+        user : process.env.MAIL_USER,
+        pass: process.env.MAIL_TOKEN
+    }
+})
+
+
+transporter.verify().then(()=>{
+    console.log('Ready for send emails.')
+})
+
+export {transporter}
--- src/network/routes/prenotification.ts
+++ src/network/routes/prenotification.ts
@@ -1,10 +1,30 @@
 import { NextFunction, Router } from "express";
 import { response } from "network/response";
 import { PrenotificationService } from "services/prenotification";
-import { idSchema} from "schemas";
+import { idSchema,storePrenotification} from "schemas";
 import { validatorCompiler } from "./utils";
 
 
 const Prenotification = Router();
+
+Prenotification.route('/prenotification')
+    .post(validatorCompiler(storePrenotification,"body"),
+    async(
+        req:CustomRequest,
+        res:CustomResponse,
+        next:NextFunction
+    ):Promise<void>=>{
+        try {
+            const {
+                body: { args },
+              } = req;
+              const pn=new PrenotificationService({prenotification:args})
+              const result = await pn.process({type:'store'})
+              response({error:false,message:result,res,status:201})
+        } catch (error) {
+            next(error);
+        }
+    })
+
 
 export {Prenotification}
